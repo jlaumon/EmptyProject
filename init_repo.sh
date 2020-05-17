99fffbef1d4d7467afe1892c6771bd5eb93e25cd
jeremy <jeremy.laumon@gmail.com>
2020-05-17 14:40:41 +0200

    Remove submodule dir first or it won't init

--- init_repo.sh
+++ init_repo.sh
@@ -9,6 +9,7 @@ git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
     do
         url_key=$(echo $path_key | sed 's/\.path/.url/')
         url=$(git config -f .gitmodules --get "$url_key")
+	rmdir $path
         git submodule add $url $path
     done
 	
