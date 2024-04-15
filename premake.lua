77ee23867b18cfa5b8d22040ff60723f78e1678c
Jeremy <jeremy.laumon@gmail.com>
2024-04-15 17:26:43 +0200

    Added more sensible configurations to premake.lua
    Also fixed mixed space and tabs, boo.

--- premake.lua
+++ premake.lua
@@ -1,63 +1,88 @@
 solution "EmptyProject"
 	
 	platforms { "x64" }
-	configurations { "Debug", "Release" }
+	configurations { "Debug", "DebugASAN", "DebugOpt", "Release" }
 	startproject "EmptyProject"
 
 	project "EmptyProject"
 
-        kind "ConsoleApp"
-        symbols "On"
-        editandcontinue "On"
-        dpiawareness "HighPerMonitor"
-        
-        defines
-        {
-            "_CRT_SECURE_NO_WARNINGS",
-            "STR_DEFINE_STR32",
-            "STR_SUPPORT_STD_STRING=0",
-        }
+		kind "ConsoleApp"
+		symbols "On"
+		cppdialect "C++20"
+		dpiawareness "HighPerMonitor"
+		flags 
+		{
+			"MultiProcessorCompile",
+			"FatalWarnings"
+		}
+		
+		defines
+		{
+			"_CRT_SECURE_NO_WARNINGS",
+			"STR_DEFINE_STR32",
+			"STR_SUPPORT_STD_STRING=0",
+		}
 
-        filter { "toolset:msc*" }
-            disablewarnings
-            {
-                "4244", -- 'return': conversion from '__int64' to 'int', possible loss of data
-                "4267", -- 'initializing': conversion from 'size_t' to 'int', possible loss of data
-            }
+		filter { "toolset:msc*" }
+			disablewarnings
+			{
+				"4244", -- 'return': conversion from '__int64' to 'int', possible loss of data
+				"4267", -- 'initializing': conversion from 'size_t' to 'int', possible loss of data
+			}
 
-        filter { "configurations:Debug" }
-            
-
-        filter {}
-        
+		filter { "configurations:Debug" }
+			targetsuffix "Debug"
+			defines "ASSERTS_ENABLED"
+			optimize "Debug"
+			editandcontinue "On"
+			
+		filter { "configurations:DebugASAN" }
+			targetsuffix "DebugASAN"
+			defines "ASSERTS_ENABLED"
+			optimize "Debug"
+			editandcontinue "Off"	  -- incompatble with ASAN
+			flags "NoIncrementalLink" -- incompatble with ASAN
+			sanitize "Address"
+			
+		filter { "configurations:DebugOpt" }
+			targetsuffix "DebugOpt"
+			defines "ASSERTS_ENABLED"
+			optimize "Full"
+			editandcontinue "On"
+			
+		filter { "configurations:Release" }
+			optimize "Full"
+			
+		filter {}
+		
 		files 
 		{
-            "src/.clang-format",
+			"src/.clang-format",
 			"src/**.h",
-            "src/**.cpp",
-            "src/**.natvis",
-            "thirdparty/imgui/*.h",
-            "thirdparty/imgui/*.cpp",
-            "thirdparty/imgui/**.natvis",
-            "thirdparty/imgui/backends/imgui_impl_dx11.cpp",
-            "thirdparty/imgui/backends/imgui_impl_dx11.h",
-            "thirdparty/imgui/backends/imgui_impl_win32.cpp",
-            "thirdparty/imgui/backends/imgui_impl_win32.h",
-            "thirdparty/Str/**.h",
-            "thirdparty/Str/**.cpp",
-            "thirdparty/Str/**.natvis",
-            "thirdparty/icons/**.h",
-        }
+			"src/**.cpp",
+			"src/**.natvis",
+			"thirdparty/imgui/*.h",
+			"thirdparty/imgui/*.cpp",
+			"thirdparty/imgui/**.natvis",
+			"thirdparty/imgui/backends/imgui_impl_dx11.cpp",
+			"thirdparty/imgui/backends/imgui_impl_dx11.h",
+			"thirdparty/imgui/backends/imgui_impl_win32.cpp",
+			"thirdparty/imgui/backends/imgui_impl_win32.h",
+			"thirdparty/Str/**.h",
+			"thirdparty/Str/**.cpp",
+			"thirdparty/Str/**.natvis",
+			"thirdparty/icons/**.h",
+		}
 		
 		includedirs 
 		{
-            "src",
-            "thirdparty",
-            "thirdparty/imgui",
-            "thirdparty/imgui/backends",
-        }
-        
-        links
-        {
-            "D3D11.lib",
-        }
+			"src",
+			"thirdparty",
+			"thirdparty/imgui",
+			"thirdparty/imgui/backends",
+		}
+		
+		links
+		{
+			"D3D11.lib",
+		}
